93bbee417f6e0fc6f4b64b7172e93c2cd8fd0fb0
Matthew Harrington <mattharrington576@gmail.com>
2016-10-18 15:49:37 -0400

    Much stuck, very help

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,21 +1,40 @@
 var $ = require('jquery');
 var Backbone = require('backbone');
 
-var MyModel = Backbone.Model.extend({
+var MyPost = Backbone.Model.extend({
   alpha: function(){
     console.log('Lorem ipsum dolor sit amet.');
   }
 });
 
-var MyModelCollection= Backbone.Collection.extend({
-  model: MyModel,
-  url: 'https://undefined.url'
+var MyPostCollection = Backbone.Collection.extend({
+  model: MyPost,
+  url: 'http://tiny-lasagna-server.herokuapp.com/collections/posts/',
+  parse: function(data){
+    return data;
+  }
 });
 
-var coolModels = new MyModelCollection();
+var button = document.getElementById('the-button');
+var publicPosts = new MyPostCollection();
 
-//coolModels.fetch().then(function(){});
+$(button.on('click', function(e){
+  e.preventDefault();
 
-$("#the-button")[0];
-$("#the-button")[0].disabled = false;
+  button.disabled = true;
+
+  publicPosts.fetch().then(function(){
+
+    // publicPosts.forEach(
+    // document.createElement('<li>')
+    // );
+
+    console.log(publicPosts);
+    button.disabled = false;
+  });
+}));
+
+
+
+//$("#the-button")[0].disabled = false;
 //$("#the-button")[0].disabled = true;
