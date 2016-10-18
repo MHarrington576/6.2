b1f8d136e13a738b382d1192297b19bc6636fdf4
Matthew Harrington <mattharrington576@gmail.com>
2016-10-18 14:19:53 -0400

    Prepared for AJAX request

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+var $ = require('jquery');
+var Backbone = require('backbone');
+
+var MyModel = Backbone.Model.extend({
+  alpha: function(){
+    console.log('Lorem ipsum dolor sit amet.');
+  }
+});
+
+var MyModelCollection= Backbone.Collection.extend({
+  model: MyModel,
+  url: 'https://undefined.url'
+});
+
+var coolModels = new MyModelCollection();
+
+//coolModels.fetch().then(function(){});
+
+$("#the-button")[0];
+$("#the-button")[0].disabled = false;
+//$("#the-button")[0].disabled = true;
